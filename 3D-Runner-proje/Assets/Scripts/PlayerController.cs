d00f30bb6ad241ed90dbe2d2516bf9bdc6862632
Yalçın Yağız KORKMAZ <99255561+yalcinyagizkorkmaz@users.noreply.github.com>
2023-07-07 23:33:45 +0300

    3-D-Runner-Proje
    3-D-Oyun-Projesi

---
+++ 3D-Runner-proje/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+    public float runningSpeed;
+    float touchXDelta=0;
+    float newX=0;
+    public float xSpeed;
+    public float limitx;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      SwipeCheck();
+    }
+
+    private void SwipeCheck()
+    {
+          if(Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Moved)//input control yani dokunduğumuzu
+        {
+           //Debug.Log(Input.GetTouch(0).deltaPosition.x / Screen.width);
+           touchXDelta=Input.GetTouch(0).deltaPosition.x/ Screen.width;
+        }
+        else if(Input.GetMouseButton(0))
+        {
+           touchXDelta=Input.GetAxis("Mouse X");//project setting input managerdan ismini değiştirebiliriz
+        }
+        else
+        {
+         touchXDelta=0;
+        }
+        newX=transform.position.x + xSpeed * touchXDelta *Time.deltaTime;
+        newX=Mathf.Clamp(newX,-limitx,limitx);//sağa ve sola limit değeri
+        
+
+
+         Vector3 newPosition=new Vector3(newX,transform.position.y,transform.position.z+runningSpeed*Time.deltaTime);
+        transform.position=newPosition;
+    }
+}
